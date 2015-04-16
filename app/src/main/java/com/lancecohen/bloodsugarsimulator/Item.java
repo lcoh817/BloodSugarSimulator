9631d325ac17ef6f8a25be65a4f573dad7a2a9ed
Lance Cohen <lcoh817@gmail.com>
2015-04-16 01:42:07 -0700

    Added class Item as model of each item in list view for an adaptor.

---
+++ app/src/main/java/com/lancecohen/bloodsugarsimulator/Item.java
@@ -0,0 +1,24 @@
+package com.lancecohen.bloodsugarsimulator;
+
+/**
+ * Created by lancecohen on 16/04/15.
+ */
+public class Item {
+
+    private String item;
+
+    public Item(String item) {
+        super();
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+
+}
